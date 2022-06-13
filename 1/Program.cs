47e64a2f73ed78def769f03d16ecd5cfb34e9a62
Stanislav <hansolo99@yandex.ru>
2022-06-13 10:15:55 +0300

    First exercise complite

--- 1/Program.cs
+++ 1/Program.cs
@@ -4,22 +4,12 @@
 // a[1]=2
 // a[2]=3
 
-void printArray(int[]array)
-{
-    for (int i = 0; i < array.Length; i++)
+Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+    while(true)
     {
-        System.Console.WriteLine(array[i]);
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
     }
-}
-
-Console.WriteLine("Введите трехзначное число ->");
-string number = Console.ReadLine();
-int numberInt = int.Parse(number);
-
-int [] array = new int[3];
-
-array[0] = numberInt / 100;
-array[1] = numberInt % 100 / 10;
-array[2] = numberInt % 10;
-
-printArray(array);
