a4b9ff84a1651dbe7ecb729c1cb218f8ad416879
Stanislav <hansolo99@yandex.ru>
2022-06-13 09:45:46 +0300

    First Commit

---
+++ 1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Дано 3-хзначное число, 
+// пример: 123 => {1, 2. 3}
+// a[0]= 1
+// a[1]=2
+// a[2]=3
+
+void printArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
+
+Console.WriteLine("Введите трехзначное число ->");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+
+int [] array = new int[3];
+
+array[0] = numberInt / 100;
+array[1] = numberInt % 100 / 10;
+array[2] = numberInt % 10;
+
+printArray(array);
